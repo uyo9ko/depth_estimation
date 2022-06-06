1dbc7650ed7746ff1afa1e2c80c48eb7063cffce
uy9ko <uy9ko@gmail.com>
2022-06-06 14:02:31 +0000

    ibn_val

--- densemodel.py
+++ densemodel.py
@@ -10,9 +10,7 @@ from collections import OrderedDict
 import cv2
 import os
 import numpy as np
-from cov_settings import CovMatrix_ISW
-from instance_whitening import *
-
+from densenet_ibn import densenet169_ibn_a
 
 class UpSample(nn.Sequential):
     def __init__(self, input_channel, output_channel):
@@ -31,7 +29,7 @@ class UpSample(nn.Sequential):
 
 
 class Decoder(nn.Module):
-    def __init__(self, num_features=2208, decoder_width=0.5):
+    def __init__(self, num_features=1664, decoder_width=0.5):
         super(Decoder, self).__init__()
         features = int(num_features * decoder_width)
 
@@ -39,10 +37,10 @@ class Decoder(nn.Module):
             num_features, features, kernel_size=1, stride=1, padding=1
         )
 
-        self.up1 = UpSample(features // 1 + 384, features // 2)
-        self.up2 = UpSample(features // 2 + 192, features // 4)
-        self.up3 = UpSample(features // 4 + 96, features // 8)
-        self.up4 = UpSample(features // 8 + 96, features // 16)
+        self.up1 = UpSample(features // 1 + 256, features // 2)
+        self.up2 = UpSample(features // 2 + 128, features // 4)
+        self.up3 = UpSample(features // 4 + 64, features // 8)
+        self.up4 = UpSample(features // 8 + 64, features // 16)
         self.relu3 = nn.LeakyReLU(0.2)
         self.conv3 = nn.Conv2d(features // 16, 1, kernel_size=3, stride=1, padding=1)
 
@@ -54,6 +52,7 @@ class Decoder(nn.Module):
             features[8],
             features[11],
         )
+
         x_d0 = self.conv2(x_block4)
         x_d1 = self.up1([x_d0, x_block3])
         x_d2 = self.up2([x_d1, x_block2])
@@ -68,33 +67,20 @@ class Decoder(nn.Module):
         return self.conv3(self.relu3(x_d5))
 
 
-
 class Encoder(nn.Module):
+
     def __init__(self):
         super(Encoder, self).__init__()
-        self.original_model = models.densenet161(pretrained=True)
+        self.original_model = models.densenet169(pretrained=True)
+        # self.original_model = densenet169_ibn_a(pretrained=True)
+        # print(self.original_model.features._modules.items())
 
-    def forward(self, x, isw=False):
+    def forward(self, x):
         features = [x]
-        w_arr = []
         for k, v in self.original_model.features._modules.items():
-            if isw:
-                if k == "norm0":
-                    out = features[-1]
-                    out, w = InstanceWhitening(out.shape[1])(out)
-                    w_arr.append(w)
-                elif k == "denseblock1" or k == "denseblock2":
-                    out = v(features[-1])
-                    out, w = InstanceWhitening(out.shape[1])(out)
-                    w_arr.append(w)
-                else:
-                    out = v(features[-1])
-                features.append(out)
-            else:
-                features.append(v(features[-1]))
-        if isw:
-            return features, w_arr
+            features.append(v(features[-1]))
         return features
+
 
 
 class DenseDepth(nn.Module):
@@ -126,7 +112,7 @@ class MyModel(LightningModule):
         # self.SiLog = SiLogLoss()
 
         self.model = DenseDepth()
-        self.load_state_dict(torch.load(ckpt_path)["state_dict"])
+        self.model.load_state_dict(torch.load(ckpt_path)["state_dict"])
 
     def forward(self, x):
         self.model.encoder.eval()
